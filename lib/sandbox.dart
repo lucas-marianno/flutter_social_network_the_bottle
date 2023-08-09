2284c01774cb73b9bc158b9b45f44cee7336df2e
Lucas Moraes <lucas.pk16@outlook.com>
2023-08-09 13:29:06 -0300

    added support to web for image upload and download

--- lib/sandbox.dart
+++ lib/sandbox.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:the_wall/components/elevated_button.dart';
@@ -14,7 +15,7 @@ class Sandbox extends StatefulWidget {
 
 class _SandboxState extends State<Sandbox> {
   List<String> imageList = [];
-  bool isLoading = true;
+  bool isLoading = false;
 
   Future<void> uploadImage(ImageSource imageSource) async {
     setState(() => isLoading = true);
@@ -24,7 +25,13 @@ class _SandboxState extends State<Sandbox> {
 
     String newFileName = 'Test Folder/img-${DateTime.now()}';
 
-    final uploadTask = await FirebaseStorage.instance.ref(newFileName).putFile(File(image.path));
+    late final TaskSnapshot uploadTask;
+    if (kIsWeb) {
+      uploadTask =
+          await FirebaseStorage.instance.ref(newFileName).putData(await image.readAsBytes());
+    } else {
+      uploadTask = await FirebaseStorage.instance.ref(newFileName).putFile(File(image.path));
+    }
     final newFileUrl = await uploadTask.ref.getDownloadURL();
 
     FirebaseFirestore.instance.collection('Test Collection').add({
@@ -66,6 +73,14 @@ class _SandboxState extends State<Sandbox> {
                         return Padding(
                           padding: const EdgeInsets.all(5.0),
                           child: Image.network(snapshot.data!.docs[index].data()['imagePath']),
+
+                          /// When loading an image in a web app you need to allow (server side)
+                          /// your database contents to be displayed cross origin.
+                          /// -> CORS - Cross Origing Resource Sharing
+                          /// this tutorial explains how to do it:
+                          /// https://stackoverflow.com/questions/65653801/flutter-web-cant-load-network-image-from-another-domain
+                          /// but be careful because any website can access your resources, which
+                          /// can lead security leaks (and atacks from bots).
                         );
                       },
                     );
