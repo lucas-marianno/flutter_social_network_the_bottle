62aa65000f4809d80717dd69f5c421bca039d129
Lucas Moraes <lucas.pk16@outlook.com>
2023-10-03 14:19:07 -0300

    README.md update

--- README.md
+++ README.md
@@ -19,6 +19,36 @@ Completely developed with Flutter and Firebase by [Lucas Marianno](https://linkt
 - Like comments
 - Edit your profile page
 - Send individual messages
+
 ##
 ### Read the latest [Release Notes](https://github.com/lucas-marianno/The-Bottle/releases) for detailed and up to date info on features!
 ##
+##
+##
+
+___
+# App Preview
+
+### Post feed
+![01](lib/assets/screenshots/01.jpeg)
+___
+### Dark and Light Mode
+![02](lib/assets/screenshots/02.jpeg)
+___
+### Comments and Likes
+![03](lib/assets/screenshots/03.jpeg)
+___
+### Conversations
+![04](lib/assets/screenshots/04.jpeg)
+___
+### Private messages, photos and links
+![05](lib/assets/screenshots/05.jpeg)
+___
+### Like and reply to private messages
+![06](lib/assets/screenshots/06.jpeg)
+___
+### Multiple pages
+![07](lib/assets/screenshots/07.jpeg)
+___
+### Choose your username and biography
+![08](lib/assets/screenshots/08.jpeg)
--- lib/sandbox.dart
+++ lib/sandbox.dart
@@ -1,8 +1,6 @@
-// ignore_for_file: avoid_print
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:the_bottle/pages/about_page.dart';
 import 'package:the_bottle/theme.dart';
-import 'package:the_bottle/util/timestamp_to_string.dart';
 
 const sandboxEnabled = false;
 
@@ -16,21 +14,7 @@ class Sandbox extends StatelessWidget {
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: ThemeMode.dark,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('S A N D B O X')),
-        body: const Center(
-          child: ElevatedButton(onPressed: test, child: Text('print')),
-        ),
-      ),
+      home: const AboutPage(),
     );
   }
-}
-
-void test() async {
-  final data =
-      await FirebaseFirestore.instance.collection('User Posts').doc('ZvGElJdLEgIeD6FyvVCa').get();
-  final timestamp = data.data()?['TimeStamp'] as Timestamp;
-  print(timestamp);
-  print(timestampToString(timestamp));
-  print(timestampToString(timestamp, absolute: true));
 }
